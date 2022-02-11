898c27cff9d9a4f81c1e56044b767fa139cd86d4
Tinashe Kachikoti <tk21514@essex.ac.uk>
2022-02-11 19:01:10 +0000

    update comments

--- question_2.sql
+++ question_2.sql
@@ -1,3 +1,6 @@
+-- Count the number of people in department 30 who receive a
+-- salary and the number of people who receive a commission
+
 USE COMPANY1;
 
 SELECT DEPTNO, COUNT(*) AS PEOPLE, COUNT(SAL) AS PEOPLE_WITH_SAL, COUNT(COMM) AS PEOPLE_WITH_COMM
